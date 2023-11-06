bac6fd2970351250527bf538f35fef025ade8d26
WebGCDev <gaetano.chi@gmail.com>
2023-11-06 18:41:58 +0100

    feat : Soluzione finale 2 con formula matematica semplice

--- js/myscript.js
+++ js/myscript.js
@@ -5,12 +5,12 @@ const etaAnagrafica = parseInt(prompt('Inserisci la tua età'));
 let prezzoBiglietto = kmDaPercorrere * 0.21;
 
 if (etaAnagrafica < 18) {
-  prezzoBiglietto = prezzoBiglietto * 0.8;
+  prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.2;
   console.log(
     'Il prezzo del biglietto scelto è: ' + prezzoBiglietto.toFixed(2)
   );
 } else if (etaAnagrafica >= 65) {
-  prezzoBiglietto = prezzoBiglietto * 0.6;
+  prezzoBiglietto = prezzoBiglietto - prezzoBiglietto * 0.4;
 
   console.log(
     'Il prezzo del biglietto scelto è: ' + prezzoBiglietto.toFixed(2)
